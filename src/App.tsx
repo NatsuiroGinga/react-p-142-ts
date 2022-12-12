dcb2c531480a4694cca6989d7be0d3999b19b928
NatsuiroGinga <duyueyeweiliang@gmail.com>
2022-12-12 12:32:14 +0800

    channel.Store

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "@/App.css"
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import WrapperRoutes from "@/router";
 
 const App: React.FC = () => {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <WrapperRoutes/>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <WrapperRoutes/>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
